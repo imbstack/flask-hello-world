cca0941f8baf6d2145820af7edab352743a5cc15
Brian Stack <brian@render.com>
2023-01-26 12:12:43 -0800

    With 301s

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/redir')
+def redir():
+    return redirect('/foo', code=301)
+
+@app.route('/foo')
+def foo():
+    return 'FOO'
