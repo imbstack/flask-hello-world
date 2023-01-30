e5e9eb40c9bb82eb811149b21ea8b41f3605a901
Brian Stack <brian@render.com>
2023-01-30 15:48:04 -0800

    Show args

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! ' + request.args.get('aaa', 'NOT WORKING')
 
 @app.route('/redir')
 def redir():
