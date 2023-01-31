cdc651973210ab61cd18947df24ae755aa4b38b1
Brian Stack <brian@render.com>
2023-01-31 10:34:46 -0800

    sockets

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
-from flask import Flask, redirect, request
-app = Flask(__name__)
+from aiohttp import web
+import socketio
+from datetime import datetime
+import asyncio
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World! ' + request.args.get('aaa', 'NOT WORKING')
+sio = socketio.AsyncServer()
+app = web.Application()
+runner = web.AppRunner(app)
+sio.attach(app)
 
-@app.route('/redir')
-def redir():
-    return redirect('/foo', code=301)
+async def index(request):
+    """Serve the client-side application."""
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
-@app.route('/foo')
-def foo():
-    return 'FOO'
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.event
+async def chat_message(sid, data):
+    print("message ", data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+app.router.add_get('/', index)
+
+async def tick():
+    tt = datetime.now().isoformat()
+    await sio.emit('tick', {'data': tt})
+    print(tt)
+    await asyncio.sleep(2)
+    await tick()
+
+async def main():
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 5000)
+    await site.start()
+    while True:
+        await tick()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
