eb297b9e5f0edb9a950f98c416b6e97083eb1135
Brian Stack <brian@render.com>
2023-09-07 10:43:02 -0700

    Forward requests to upstream

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return requests.get(os.environ["UPSTREAM"]).text
